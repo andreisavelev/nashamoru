57df6b1eaa29ce7e72309a466ed270e785914b25
Andrey Savelyev <savelevcorr@gmail.com>
2015-11-06 19:48:32 +1000

    Installed bower

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require("gulp");
+var connect require("gulp-connect");
+var opn = require("opn");
+var min = require("gulp-uglify");
+var less = require("gulp-less");
+var concat = require("gulp-concat");
