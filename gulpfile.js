e7711d8451c37870145527541d9fb4819d29396d
Andrey Savelyev <savelevcorr@gmail.com>
2015-11-06 20:01:17 +1000

    less init

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require("gulp");
-var connect require("gulp-connect");
+var connect = require("gulp-connect");
 var opn = require("opn");
 var min = require("gulp-uglify");
 var less = require("gulp-less");
 var concat = require("gulp-concat");
+
+// Compile less
+gulp.task('less', function () {
+	return gulp.src('./frontend/src/css/*.less')
+		.pipe(less({
+			paths: [ path.join(__dirname, 'less', 'includes') ]
+		}))
+		.pipe(gulp.dest('./frontend/app/css'));
+});
